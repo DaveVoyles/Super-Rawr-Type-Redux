119fcdecc03502ff0e2c379d14d65d4d591ace98
Dave Voyles <Dnvoyles@gmail.com>
2013-11-04 08:05:26 -0500

    Removed vincent's files from Git

--- tools/bake.bat
+++ tools/bake.bat
@@ -21,3 +21,33 @@ php tools/bake.php %IMPACT_LIBRARY% %GAME% %OUTPUT_FILE%
 ::c:/php/php.exe tools/bake.php %IMPACT_LIBRARY% %GAME% %OUTPUT_FILE%
 
 pause
+
+
+
+
+@echo off
+
+
+
+:: Path to impact.js and your game's main .js
+
+SET IMPACT_LIBRARY=C:\Users\dvoyle200\SkyDrive\Development\web\impact\impact.js
+
+ 
+SET GAME=C:\Users\dvoyle200\SkyDrive\Development\web\impact\lib\game\main.js
+
+
+
+:: Output file
+SET OUTPUT_FILE=game.min.js
+
+
+
+
+:: Change CWD to Impact's base dir and bake!
+cd C:\Users\dvoyle200\SkyDrive\Development\web\impact
+
+C:\wamp\bin\php\php5.3.13\php.exe tools/bake.php %IMPACT_LIBRARY% %GAME% %OUTPUT_FILE%
+C:\Users\dvoyle200\SkyDrive\Development\web\impact
+
+pause
